3bfd71ce32face5c1fcbff76e9298511c6e10b01
Ben <ben@pez.(none)>
2009-10-15 23:33:04 -0700

    added user filtering, and home page

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,5 +1,19 @@
 class FlickrController < ApplicationController
   def all
+    if params[:user]
+      begin
+        user_id = flickr.people.findByUsername(:username => params[:user]).id
+        @photos = flickr.people.getPublicPhotos({:user_id => user_id, :per_page => (params[:id] || 10), :extras => 'url_s'})
+      rescue
+        get_recent_photos
+      end
+    else
+      get_recent_photos
+    end
+  end
+  
+  protected
+  def get_recent_photos
     @photos = flickr.photos.getRecent({:extras => 'url_s', :per_page => (params[:id] || 10)})
   end
 end
