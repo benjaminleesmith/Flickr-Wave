0361590d9a6eab071baab703d44efe9fa283eefe
Ben <ben@pez.(none)>
2009-11-10 22:25:20 -0700

    added support for user_id

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,8 +1,8 @@
 class FlickrController < ApplicationController
   def all
-    if params[:user]
+    if params[:user] || params[:user_id]
       begin
-        user_id = flickr.people.findByUsername(:username => params[:user]).id
+        user_id = params[:user_id] || flickr.people.findByUsername(:username => params[:user]).id
         @photos = flickr.people.getPublicPhotos({:user_id => user_id, :per_page => (params[:id] || 10), :extras => 'url_s'})
       rescue
         get_recent_photos
